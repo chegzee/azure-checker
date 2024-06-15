cffbde74fbb69e87d8ea5273c0db1f8ec5d2c671
isaac segun <chegzee1@gmail.com>
2024-06-15 18:28:16 +0100

    another update

--- index.php
+++ index.php
@@ -9,9 +9,6 @@
 
     <body>
         <h2>HELLO TESTING</h2>
-        <h2>AM GOOD TO GO</h2>
-        <P>HELLO EVERYONE</P>
-        i want to play game
-        scared info
+        <h2>FROM ISOSIT</h2>
     </body>
 </html>
