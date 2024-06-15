4b6a4920696194eaee56cf1428952c37a9fc1b29
isaac segun <chegzee1@gmail.com>
2024-06-15 13:17:51 +0100

    first update

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "hello world";
+?>
+
+<html>
+
+    <head>TEST</head>
+
+    <body>
+        <h2>HELLO TESTING</h2>
+        <h2>AM GOOD TO GO</h2>
+        <P>HELLO EVERYONE</P>
+    </body>
+</html>
